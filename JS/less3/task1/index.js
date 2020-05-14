1a20c6347ca1c97c4e512f6fbc6b75bfad2d8b9b
unknown <shotvideo25@gmail.com>
2020-05-14 12:29:11 +0300

    ddd

--- JS/less3/task1/index.js
+++ JS/less3/task1/index.js
@@ -9,16 +9,16 @@ console.log('7 - null: ', 7 - null);
 console.log('5 - undefined: ', 5 - undefined);
 console.log('\'text\' + null: ', 'text' + null);
 let num = 17;
-console.log('++num : ', ++num);
-console.log('num++ :', num++);
+console.log('++num: ', ++num);
+console.log('num++: ', num++);
 
 const a = 17;
 const b = -a;
 console.log('-a: ', -a);
 
-console.log('\'24\'/ 4: ', '24'/4);
+console.log('\'24\' / 4: ', '24' / 4);
 console.log('18 * \'3\': ', 18 * '3');
-console.log('\'9\'-\'3\': ', '9' - '3');
+console.log('\'9\' - \'3\' : ', '9' - '3');
 console.log('\'seventeen\' / 3: ', 'seventeen' / 3);
 console.log('4 * \'four\': ', 4 * 'four');
 console.log('17 / 0: ', 17 / 0);
