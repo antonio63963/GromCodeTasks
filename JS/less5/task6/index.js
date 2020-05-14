bfe68b81bdf1cbb0e9b075c57e06d83c0cc1731b
unknown <shotvideo25@gmail.com>
2020-05-14 22:27:13 +0300

    math

--- JS/less5/task6/index.js
+++ JS/less5/task6/index.js
@@ -1,8 +1,9 @@
 function getPrimes(num) {
     
     for(let i = 2; i <= num; i++) {
-        if(i % i === 0 && i % 1 === 0) {
+        if(i % i === 0 && i % 1 === 0 && Math.sign(i) === 1) {
             console.log(i);
+            // console.log(Math.sign(i));
         } else {
             continue;
         }
