99c47578b3737ae96cbfadbd82f699f57194d26b
unknown <shotvideo25@gmail.com>
2020-05-14 17:46:08 +0300

    %5

---
+++ JS/less4/task5/index.js
@@ -0,0 +1,21 @@
+const m = 1;
+const n = 20;
+let result = 0;
+
+for(let i = m; i <= n; i++) {
+    if(i % 2 == 0 && i % 4 !== 4 && i % 5 !== 0) {
+        result += i;
+    }
+    else if(i % 3 === 0 && i % 5 !== 0) {
+        result -= i;
+    }
+    else if(i % 4 === 0 && i % 5 !== 0) {
+        result *= i;
+    }
+    else if(i % 5 === 0) {
+        console.log(i);
+        continue;
+    }
+    else {continue;}
+
+}
