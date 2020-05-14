bba904eabfd36da4bf3f06c7b0a39f63d7353fc2
unknown <shotvideo25@gmail.com>
2020-05-14 17:20:32 +0300

    x

--- JS/less4/task3/index.js
+++ JS/less4/task3/index.js
@@ -3,6 +3,6 @@
 for(let i = 1; i <= 5; i++) {
     console.log('\n=> ' + i + ' <=\n\n');
     for(let n = 1; n <= 10; n++) {
-        console.log(i + ' * ' + n  + ' = ' + i*n);
+        console.log(i + ' x ' + n  + ' = ' + i*n);
     }
 }
