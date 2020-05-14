383cd5f2ccffc945057a862a11a1acbc1e4b664d
unknown <shotvideo25@gmail.com>
2020-05-14 18:43:32 +0300

    break

--- JS/less4/task5/index.js
+++ JS/less4/task5/index.js
@@ -5,12 +5,15 @@ let result = 0;
 for(let i = m; i <= n; i++) {
     if(i % 2 == 0 && i % 4 !== 4 && i % 5 !== 0) {
         result += i;
+        break;
     }
     else if(i % 3 === 0 && i % 5 !== 0) {
         result -= i;
+        break;
     }
     else if(i % 4 === 0 && i % 5 !== 0) {
         result *= i;
+        break;
     }
     else if(i % 5 === 0) {
         console.log(i);
