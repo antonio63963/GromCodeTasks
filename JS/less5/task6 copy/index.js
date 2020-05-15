7ac3b649a191b3f9502679985489d5971b939818
unknown <shotvideo25@gmail.com>
2020-05-15 17:17:02 +0300

    without laibles

---
+++ JS/less5/task6 copy/index.js
@@ -0,0 +1,38 @@
+// function getPrimes(num) {
+//     Primes: for (let i = 2; i <= num; i++) {
+
+//         for (let j = 2; j < i; j++) {
+//             if (i % j == 0) {
+//                 continue Primes;
+//             }
+//         }
+//         console.log(i);
+
+//     }
+
+// }
+// console.log(getPrimes(17));
+function isPrimeNum(num) {
+    let rightNum = true;
+    for (let j = 2; j < num; j++) {
+        if (num % j == 0) {
+            rightNum = false;
+            continue;
+        }
+    }
+    if (rightNum === true) {
+        // console.log(num);
+        return num;
+    }
+}
+
+// let num = isPrimeNum(3);
+// console.log(num);
+
+function getPrimes(num) {
+    for (let i = 2; i <= num; i++) {
+        console.log(isPrimeNum(i));
+    }
+}
+
+// getPrimes(20);
