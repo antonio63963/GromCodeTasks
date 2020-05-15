6ab0fe9969ded2cf8307988c43c9bfa01ac8c553
unknown <shotvideo25@gmail.com>
2020-05-15 14:09:20 +0300

    division

---
+++ JS/less5/task9/index.js
@@ -0,0 +1,17 @@
+
+function findDivCount(a, b, n) {
+    let count = 0;
+
+    for(let i = a; i < b; i++) {
+        if(i % n === 0) {
+            count ++;
+            // console.log(i);
+        }
+    }
+    return count;
+}
+
+// console.log(findDivCount(10, 20, 2));
+
+
+
