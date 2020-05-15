5e44c32c2f47e85b18adb5f86a8dc9aefcba99cb
unknown <shotvideo25@gmail.com>
2020-05-15 21:51:48 +0300

    reverse2

--- JS/less6/task9/index.js
+++ JS/less6/task9/index.js
@@ -1,9 +1,9 @@
-let arr = [10, 2, 3, 4];
+// let arr = [10, 2, 3, 4];
 function reverseArray(arr) {
        if(Array.isArray(arr)) {
        return arr.reverse();
     } else { return null;}
 }
 
-let newArr = reverseArray(arr);
-console.log(newArr);
+// let newArr = reverseArray(arr);
+// console.log(newArr);
