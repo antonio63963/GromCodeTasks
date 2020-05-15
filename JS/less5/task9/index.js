69075b066b74d9c8cc0b1b325926656a543ac2a9
unknown <shotvideo25@gmail.com>
2020-05-15 14:11:33 +0300

    edge

--- JS/less5/task9/index.js
+++ JS/less5/task9/index.js
@@ -2,7 +2,7 @@
 function findDivCount(a, b, n) {
     let count = 0;
 
-    for(let i = a; i < b; i++) {
+    for(let i = a; i <= b; i++) {
         if(i % n === 0) {
             count ++;
             // console.log(i);
