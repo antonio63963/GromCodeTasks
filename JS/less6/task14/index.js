d1b72fe47ed2070c05438aeed1b34520057ff075
unknown <shotvideo25@gmail.com>
2020-05-16 17:37:12 +0300

    num -1

--- JS/less6/task14/index.js
+++ JS/less6/task14/index.js
@@ -1,8 +1,11 @@
 
 function getSubArray(arr, num) {
     let subArr = [];
-    for(let i = 0; i <= num; i++) {
-        subArr.push(i);
+    for(let i = 0; i <= num -1; i++) {
+        subArr.push(arr[i]);
     }
     return subArr;
 }
+
+let arr = [10, 3, 56, 66];
+console.log(getSubArray(arr, 3));
