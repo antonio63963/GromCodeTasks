3eb693b2974aaa5b3d51e7bc7fb9f56d2a827c97
unknown <shotvideo25@gmail.com>
2020-05-17 23:06:02 +0300

    balances

--- JS/less7/task9/index.js
+++ JS/less7/task9/index.js
@@ -1,7 +1,7 @@
 let clients = ['Ann', 'Bob', 'Jane', 'Mik'];
 let balances = [123, 345, 1400, 4507];
 
-const widthdraw = (clients, balances, client, amount) => 
+const withdraw = (clients, balances, client, amount) => 
     clients.indexOf(client) !== -1 &&
         amount <= balances[clients.indexOf(client)] ?
         balances[clients.indexOf(client)] -= amount :
@@ -10,4 +10,5 @@ const widthdraw = (clients, balances, client, amount) =>
 
 
 // let a = clients.indexOf('Bob');
- console.log(widthdraw(clients, balances, 'Bob', 450));
+ console.log(withdraw(clients, balances, 'Bob', 45));
+ console.log(balances);
