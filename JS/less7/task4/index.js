0c7d19c182f4406d7f75990d3d003605356ffb51
unknown <shotvideo25@gmail.com>
2020-05-17 21:03:49 +0300

    filter + map

--- JS/less7/task4/index.js
+++ JS/less7/task4/index.js
@@ -4,7 +4,7 @@ let failedStudents = ['Bob', 'Julie'];
 const getMessagesForBestStudents = (allStudents, failedStudents) => {
     const winStudents = allStudents
         .filter(name => 
-            failedStudents.indexOf(name) !== -1)
+            failedStudents.indexOf(name) == -1)
         .map(name => 
             'Good job, ' + name
         );
