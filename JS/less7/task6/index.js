66468709ecff0203704c13f0d10a2facd18ecdf2
unknown <shotvideo25@gmail.com>
2020-05-17 21:52:01 +0300

    reverse

--- JS/less7/task6/index.js
+++ JS/less7/task6/index.js
@@ -1,10 +1,9 @@
-
-
 let arrNum = [10, 12, 3, 5, 4];
 
-const reverseArray = arrNum =>
-    Array.isArray(arrNum) ?
-    arrNum.reverse()  
-    : null;
-
+const reverseArray = arrNum => {
+    let reverse = arrNum.slice();
+    if(Array.isArray(arrNum)) {
+     return  reverse.reverse();
+     } else {return   null;} 
+};
 console.log(reverseArray(arrNum));
