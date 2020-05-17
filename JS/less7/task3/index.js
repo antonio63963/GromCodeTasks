6bdef76fdf2e8277a795bb008810b66d33752870
unknown <shotvideo25@gmail.com>
2020-05-17 20:35:24 +0300

    2 варианта

--- JS/less7/task3/index.js
+++ JS/less7/task3/index.js
@@ -1,16 +1,18 @@
 
-
+// вариант 1
 const flatArray = arr => {
     return arr.flat();
 }
 
 
-let arr = [ 10, 3, 4, [10, 5], 6, 7];
+// вариант 2
 
 // const flatArray = arr => {
 //        return arr.reduce((acc, num) => {
 //         return acc.concat(num);
 //     }, []);
 // };
+
+let arr = [ 10, 3, 4, [10, 5], 6, 7];
 console.log(flatArray(arr));
 console.log(arr);
