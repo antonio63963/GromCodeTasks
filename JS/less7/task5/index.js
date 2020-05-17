20e58a71912b41e2972324c2dbb9ede6b7ee0526
unknown <shotvideo25@gmail.com>
2020-05-17 21:13:23 +0300

    square by map

--- JS/less7/task5/index.js
+++ JS/less7/task5/index.js
@@ -1,6 +1,10 @@
 let arrNum = [10, 12, 3, 5, 4]
 
-const squareArray = arrNum => 
-    arrNum.map(num => num**2);
+const squareArray = arrNum => {
+    
+   return Array.isArray ?
+     arrNum.map(num => num**2)
+    : null;
+}
 
 console.log(squareArray(arrNum));
