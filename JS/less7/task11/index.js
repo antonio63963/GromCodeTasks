963b2db9c662cf1302936d9642630e20bf8a90cc
unknown <shotvideo25@gmail.com>
2020-05-18 15:59:23 +0300

    name.length

--- JS/less7/task11/index.js
+++ JS/less7/task11/index.js
@@ -3,7 +3,7 @@ let names = ['ann', 'bob', 'jenifer', 'antonio'];
 
 const filterNames = (arr, text) => 
       arr.filter(name => 
-        name.includes(text));
+        name.includes(text) && name.length > 5);
 
 
 console.log(filterNames(names, 'an'));
