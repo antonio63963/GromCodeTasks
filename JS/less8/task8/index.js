7691b6d68f7e0ca655571eaef4a26e253def1af2
unknown <shotvideo25@gmail.com>
2020-05-19 15:53:21 +0300

    sdfsaf

--- JS/less8/task8/index.js
+++ JS/less8/task8/index.js
@@ -20,11 +20,11 @@ const mergeObjectsV2 = (obj1, obj2) => {
   return Object.assign(obj2, obj1,);
 };
 
-const mergeObjectsV3 = (obj1, obj2) => {
+const mergeObjectsV4 = (obj1, obj2) => {
   return {...obj2, ...obj1};
 };
 
-const mergeObjectsV4 = (obj1, obj2) => {
+const mergeObjectsV3 = (obj1, obj2) => {
   return {...obj1, ...obj2};
 };
 
