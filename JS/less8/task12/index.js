a690c6f271923ecd7fed03d3c6e7ca1d8cdb9e41
unknown <shotvideo25@gmail.com>
2020-05-20 20:33:44 +0300

    done

--- JS/less8/task12/index.js
+++ JS/less8/task12/index.js
@@ -1,11 +1,11 @@
 const people = {
-  'joe': 19,
-  'Nik': 17,
-  'Ann': 20,
-  'Rick': 14,
-  'Martin': 22,
-  'Julie': 30,
-  'frank': 18,
+  joe: 19,
+  Nik: 17,
+  Ann: 20,
+  Rick: 14,
+  Martin: 22,
+  Julie: 30,
+  frank: 18,
 };
 
 const getAdults = obj => {
@@ -13,8 +13,8 @@ const getAdults = obj => {
   const keysArr = Object.keys(obj);
   
   keysArr.forEach(item => {
-    if(people[item] >= 18) {
-      resultObj[item] = people[item];
+    if(obj[item] >= 18) {
+      resultObj[item] = obj[item];
     }
   });
   return resultObj;
