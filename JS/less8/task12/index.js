b180e57e0905ee01a58032fb90cb614d53b120e4
unknown <shotvideo25@gmail.com>
2020-05-20 20:29:15 +0300

    filter object

---
+++ JS/less8/task12/index.js
@@ -0,0 +1,22 @@
+const people = {
+  joe: 19,
+  Nik: 17,
+  Ann: 20,
+  Rick: 14,
+  Martin: 22,
+  Julie: 30,
+};
+
+const getAdults = obj => {
+  let resultObj = {};
+  const keysArr = Object.keys(obj);
+  
+  keysArr.forEach(item => {
+    if(people[item] >= 18) {
+      resultObj[item] = people[item];
+    }
+  });
+  return resultObj;
+};
+
+console.log(getAdults(people));
