d477ebca19827b62faf95fd53bca28128d3e414e
unknown <shotvideo25@gmail.com>
2020-05-20 10:54:20 +0300

    return

--- JS/less8/task9/index.js
+++ JS/less8/task9/index.js
@@ -20,16 +20,28 @@ let obj3 = {
   isMarried: false,
 };
 
+let obj4 = {
+  age: 1,
+  name: 'jeen',
+  country: "Ukraine",
+  isMarried: false,
+};
+
+
 const compareObjects = (obj1, obj2) => {
   let coloneArr1 = Object.keys(obj1);
   let coloneArr2 = Object.keys(obj2);
+  let trueFalseArr = [];
 
   if (coloneArr1.length == coloneArr2.length) {
     for (let p of coloneArr1) {
-      return coloneArr2.indexOf(p) !== -1 &&
-        obj1[p] == obj2[p];
+      console.log(obj1[p], '  ', obj2[p]);
+      trueFalseArr.push(coloneArr2.indexOf(p) !== -1 &&
+        obj1[p] == obj2[p]);
     }
-  } else { return false}
-}
+  } else { return false;}
+  console.log(trueFalseArr);
+  return trueFalseArr.indexOf('false') !== -1;
+};
 
-console.log(compareObjects(obj1, obj3));
+console.log(compareObjects(obj1, obj4));
