5387e24c18526d13688a0381b4b0a20e2b87198b
unknown <shotvideo25@gmail.com>
2020-05-21 17:51:21 +0300

    sorted nested object

---
+++ JS/less9/task3/index.js
@@ -0,0 +1,27 @@
+const customers = {
+    'customer-id-1': {
+        name: 'William',
+        age: 54,
+    },
+    'customer-id-2': {
+        name: 'Tom',
+        age: 17,
+    },
+    'customer-id-3': {
+        name: 'Frank',
+        age: 30,
+    }
+};
+
+const getCustomersList = obj => 
+     Object.entries(obj)
+    .map(elem => {
+        elem[1].id = elem[0];
+        return elem[1];
+    })   
+    .sort((a, b) => a.age - b.age);
+    
+
+
+// console.log(getCustomersList(customers));
+console.log(getCustomersList(customers));
