2e3008b3f2fef7432ef55d8db40f018da83cbdf8
unknown <shotvideo25@gmail.com>
2020-05-22 12:33:23 +0300

    hz

--- JS/less10/task1/index.js
+++ JS/less10/task1/index.js
@@ -1,23 +1,24 @@
-const arr = [12, 4.12, 3, 99.01, true, undefined, NaN, 'test', '123', null];
+// const arr = [12, 4.12, 3, 99.01, true, undefined, NaN, 'test', '123', null];
 
 const getFiniteNumbers = arr =>
-arr.filter(num => Number.isFinite(num));
+    arr.filter(num => Number.isFinite(num));
 
 const getFiniteNumbersV2 = arr =>
-arr.filter(num => isFinite(num));
+    arr.filter(num => isFinite(num));
 
 const getNaN = arr =>
-arr.filter(num => Number.isNaN(num));
+    arr.filter(num => Number.isNaN(num));
 
 const getNaNV2 = arr =>
-arr.filter(num => isNaN(num));
+    arr.filter(num => isNaN(num));
 
 const getIntengers = arr =>
-arr.filter(num => Number.isInteger(num));
+    arr.filter(num => Number.isInteger(num));
 
 
-console.log(getFiniteNumbers(arr));
-console.log(getFiniteNumbersV2(arr));
-console.log(getNaN(arr));
-console.log(getNaNV2(arr));
-console.log(getIntengers(arr));
+// console.log(getFiniteNumbers(arr));
+// console.log(getFiniteNumbersV2(arr));
+// console.log(getNaN(arr));
+// console.log(getNaNV2(arr));
+// console.log(getIntengers(arr));
+// console.log(arr);
