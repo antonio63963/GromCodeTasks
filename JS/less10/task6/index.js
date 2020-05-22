fbf83afe47f15e97e40a86335b114432650040fd
unknown <shotvideo25@gmail.com>
2020-05-22 18:23:13 +0300

    wtf

--- JS/less10/task6/index.js
+++ JS/less10/task6/index.js
@@ -1,15 +1,17 @@
-const arrNum = [12, 44, 456, 100, 12.34];
+
 
 const getRandomNumbers = (length, start, end ) => {
     let arrInt = [];
+//выбираю метод для округления
     const getMethod = (start, end) =>
-        end - start > 1 ? Math.floor : Math.ceil;
-
+        end - start < 1 ? Math.floor : Math.ceil;
+// создаю массив целых чисел 
     for (let i = 0; i < length; i++) {
         arrInt.push(getMethod(start, end)(Math.random() * (end - start) + start));
         console.log(end - start);
     }
-    console.log(arrInt);
+    // проверяю, на всякий случай - вдруг Округлятор не округлил!
+  
     arrInt.forEach(item => {
         if(!Number.isInteger(item)) {
             arrInt = null;
@@ -19,4 +21,4 @@ const getRandomNumbers = (length, start, end ) => {
     
 };
 
-console.log(getRandomNumbers(8, 0, 1));
+console.log(getRandomNumbers(10, 2, 20));
