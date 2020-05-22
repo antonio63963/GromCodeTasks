cbc64539bd720e5cba8f880e129816d79ba5d220
unknown <shotvideo25@gmail.com>
2020-05-22 13:55:23 +0300

    abs num max arr is arr

--- JS/less10/task4/index.js
+++ JS/less10/task4/index.js
@@ -3,8 +3,10 @@
 const arrNum = [-12, 44, -456, 100, -12.34, '-23.5'];
 
 const getMaxAbsoluteNumber = arr => {
+    if(Array.isArray(arrNum)) {
     const absArr = arr.map(num => Math.abs(num));
     return Math.max(...absArr);
+    } else { return null;}
 };
 
 console.log(getMaxAbsoluteNumber(arrNum));
