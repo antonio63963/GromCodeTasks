fa4e012685669f2341783fe3d1e6cc2810c6c220
unknown <shotvideo25@gmail.com>
2020-05-22 20:11:26 +0300

    hzzzz

--- JS/less10/task6/index.js
+++ JS/less10/task6/index.js
@@ -1,24 +1,26 @@
-
-
-const getRandomNumbers = (length, start, end ) => {
+const getRandomNumbers = (length, start, end) => {
     let arrInt = [];
-//выбираю метод для округления
+    //выбираю метод для округления
     const getMethod = (start, end) =>
         end - start < 1 ? Math.floor : Math.ceil;
-// создаю массив целых чисел 
-    for (let i = 0; i < length; i++) {
-        arrInt.push(getMethod(start, end)(Math.random() * (end - start) + start));
-        console.log(end - start);
+    // создаю массив целых чисел 
+    if (end - start < 1 && end - start > -1) {
+        return null;
+    } else {
+        for (let i = 0; i < length; i++) {
+            arrInt.push(getMethod(start, end)(Math.random() * (end - start) + start));
+            console.log(end - start);
+        }
+        return arrInt;
     }
     // проверяю, на всякий случай - вдруг Округлятор не округлил!
 
-    arrInt.forEach(item => {
-        if(Number.isInteger(item)) {
-          return  arrInt ;
-        } else { return null}
-    });
-    return arrInt;
-    
+    // arrInt.forEach(item => {
+    //     if(Number.isInteger(item)) {
+    //       return  arrInt ;
+    //     } else { return null}
+    // });
+
 };
 
-console.log(getRandomNumbers(10, 2, 20));
+console.log(getRandomNumbers(10, 30, 30.5));
