c5c028388e692566d3cc9892a0b3647d0065b182
unknown <shotvideo25@gmail.com>
2020-05-22 12:10:32 +0300

    check numbers

---
+++ JS/less10/task1/index.js
@@ -0,0 +1,23 @@
+const arr = [12, 4.12, 3, 99.01, true, undefined, NaN, 'test', '123', null];
+
+const getFiniteNumbers = arr =>
+arr.filter(num => Number.isFinite(num));
+
+const getFiniteNumbersV2 = arr =>
+arr.filter(num => isFinite(num));
+
+const getNaN = arr =>
+arr.filter(num => Number.isNaN(num));
+
+const getNaNV2 = arr =>
+arr.filter(num => isNaN(num));
+
+const getIntengers = arr =>
+arr.filter(num => Number.isInteger(num));
+
+
+console.log(getFiniteNumbers(arr));
+console.log(getFiniteNumbersV2(arr));
+console.log(getNaN(arr));
+console.log(getNaNV2(arr));
+console.log(getIntengers(arr));
