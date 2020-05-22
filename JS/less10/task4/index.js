20f5b05a09fea882499e52ecf78e44577b888847
unknown <shotvideo25@gmail.com>
2020-05-22 14:02:49 +0300

    empty arr

--- JS/less10/task4/index.js
+++ JS/less10/task4/index.js
@@ -3,10 +3,12 @@
 const arrNum = [-12, 44, -456, 100, -12.34, '-23.5'];
 
 const getMaxAbsoluteNumber = arr => {
-    if(Array.isArray(arrNum)) {
+    
+    if(Array.isArray(arr) && arr.length > 0) {
     const absArr = arr.map(num => Math.abs(num));
     return Math.max(...absArr);
     } else { return null;}
+    
 };
 
 console.log(getMaxAbsoluteNumber(arrNum));
