f324258e24ed2a414932890048ce989f29309f64
unknown <shotvideo25@gmail.com>
2020-05-22 13:46:04 +0300

    trunc

--- JS/less10/task3/index.js
+++ JS/less10/task3/index.js
@@ -4,7 +4,7 @@ const multiRound = num => [
     Math.floor(num * 100)/100, 
     Math.round(num * 100)/100,
     Math.ceil(num * 100)/100,
-    Math.trunc(num),
+    Math.trunc(num * 100)/100,
     Number(num.toFixed(2))
    
 ];
