911e770ce21ba2ebe67043953573579f080ee701
unknown <shotvideo25@gmail.com>
2020-05-22 18:12:23 +0300

    hz

--- JS/less10/task6/index.js
+++ JS/less10/task6/index.js
@@ -3,13 +3,20 @@ const arrNum = [12, 44, 456, 100, 12.34];
 const getRandomNumbers = (length, start, end ) => {
     let arrInt = [];
     const getMethod = (start, end) =>
-        end - start > 0 ? Math.floor : Math.ceil;
+        end - start > 1 ? Math.floor : Math.ceil;
 
     for (let i = 0; i < length; i++) {
         arrInt.push(getMethod(start, end)(Math.random() * (end - start) + start));
-        // console.log(end - start);
+        console.log(end - start);
     }
+    console.log(arrInt);
+    arrInt.forEach(item => {
+        if(!Number.isInteger(item)) {
+            arrInt = null;
+        } 
+    });
     return arrInt;
+    
 };
 
-console.log(getRandomNumbers(8, 3.4, -3.5));
+console.log(getRandomNumbers(8, 0, 1));
