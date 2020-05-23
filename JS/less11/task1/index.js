9970e8e41ecfbd1baf27354df53833245d4695a3
unknown <shotvideo25@gmail.com>
2020-05-23 15:38:17 +0300

    check typeof data

--- JS/less11/task1/index.js
+++ JS/less11/task1/index.js
@@ -1,19 +1,25 @@
 let myString = 'mySuperText';
 
-const splitText = (text, amount) => {
+const splitText = (text, len) => {
     let resArr = [];
+    
+    let amount = !Number.isNaN(len) && typeof len === 'undefined' ?  10 : len;
     let count = 0;
-    while (true) {
-        let chunk = text.substr(count, amount);
-        if (chunk.length === 0) {
-            break;
+    if (typeof text === 'string') {
+        while (true) {
+            let chunk = text.substr(count, amount);
+            if (chunk.length === 0) {
+                break;
+            }
+            resArr.push(chunk[0].toUpperCase() + chunk.slice(1));
+            count += amount;
+            // console.log(chunk);
+            // console.log(resArr);
         }
-        resArr.push(chunk[0].toUpperCase() + chunk.slice(1));
-        count += amount;
-        // console.log(chunk);
-        // console.log(resArr);
+    } else {
+        return null;
     }
     return resArr.join('\n');
 };
 
-console.log(splitText(myString, 4));
+console.log(splitText('ewrqwerqewrrqwr'));
