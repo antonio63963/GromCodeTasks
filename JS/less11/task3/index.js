b5ce2517a47d522b7e16c99e74f4db7a453f0cab
unknown <shotvideo25@gmail.com>
2020-05-23 19:18:02 +0300

    function up

--- JS/less11/task3/index.js
+++ JS/less11/task3/index.js
@@ -3,19 +3,23 @@ const myString = 'sooooperLoooongStreeenggg';
 const splitString = (string, len = 10) => {
     let resArr = [];
     let startIndex = 0;
-
-    if(typeof string !== 'string')  { return null;}
+    //вычисляю колличество точек
+    let dots = (chunk, len) => {
+        if (len - chunk.length < 10) {
+            return ".".repeat(len - chunk.length);
+        }
+    };
+    if (typeof string !== 'string') {
+        return null;
+    }
     while (true) {
         let chunk = string.substr(startIndex, len);
-            if(chunk.length === 0) {
-                break;
-            }
-        let dots = (chunk, len) => {
-            if (len - chunk.length < 10) {
-              return  ".".repeat(len - chunk.length);
-            }
-        };    
-        resArr.push(chunk[0].toUpperCase() + chunk.slice(1)+dots(chunk, len));
+        if (chunk.length === 0) {
+            break;
+        }
+
+        // пушу массив и если нужны точки - добавляю  
+        resArr.push(chunk[0].toUpperCase() + chunk.slice(1) + dots(chunk, len));
         startIndex += len;
     }
     return resArr;
@@ -23,6 +27,7 @@ const splitString = (string, len = 10) => {
 
 // const dotsAmount = num =>
 //     console.log(".".repeat(num));
+
 // f(20);
 
-console.log(splitString('123123123'));
+console.log(splitString(myString));
