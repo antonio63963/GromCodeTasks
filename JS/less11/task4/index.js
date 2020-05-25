1e096025bcea7be63c5066d1c79f11fa2cfb3dad
unknown <shotvideo25@gmail.com>
2020-05-25 11:46:39 +0300

    return

--- JS/less11/task4/index.js
+++ JS/less11/task4/index.js
@@ -4,23 +4,26 @@ const myNewString = "ssedddkkkkeeennnsseeeasseee";
 const countOccurrences = (string, searchingItem) => {
     let occur = 0;
     let copyString = string;
-    if(searchingItem == '') { return null;}
-    while(true) {
-        if(copyString.indexOf(searchingItem) === -1) { break;}
-        //получаю индекс совпадения
-        const index = copyString.indexOf(searchingItem);
-        // эдичу стрингу через обрезание
-        copyString = copyString.slice(index + searchingItem.length);
-        occur += 1;
-        console.log(copyString);
+    if (searchingItem == '') {
+        return null;
+    } else {
+        while (true) {
+            if (copyString.indexOf(searchingItem) === -1) {
+                break;
+            }
+            //получаю индекс совпадения
+            const index = copyString.indexOf(searchingItem);
+            // эдичу стрингу через обрезание
+            copyString = copyString.slice(index + searchingItem.length);
+            occur += 1;
+            console.log(copyString);
+        }
     }
-    console.log(occur);
+    // console.log(occur);
+    return occur;
+
 };
 
-countOccurrences(myString, "");
-console.log(typeof '');
 
-console.log(myString.indexOf(''));
-console.log(myString.indexOf('s'));
-
+console.log(countOccurrences(myString, 'e'));
 
