af5adbebf38f5c4ab701dd567e7cf1a400939453
unknown <shotvideo25@gmail.com>
2020-05-25 13:40:58 +0300

    hz

--- JS/less11/task3/index.js
+++ JS/less11/task3/index.js
@@ -1,34 +1,6 @@
 const myString = 'sooooperLoooongStreeenggg';
 
-// const splitString = (string, len = 10) => {
-//     let resArr = [];
-//     let startIndex = 0;
-//     if (typeof string !== 'string') {
-//         return null;
-//     }
-//     while (true) {
-//         let chunk = string.substr(startIndex, len);
-//         //вычисляю колличество точек
-//         let dots = (chunk, len) => {
-//             if (len - chunk.length < 10) {
-//                 return ".".repeat(len - chunk.length);
-//             }
-//         };
-//         if (chunk.length === 0) {
-//             break;
-//         }
 
-//         // пушу массив и если нужны точки - добавляю  
-//         resArr.push(chunk[0].toUpperCase() + chunk.slice(1) + dots(chunk, len));
-//         startIndex += len;
-//     }
-//     return resArr;
-// };
-
-// const dotsAmount = num =>
-//     console.log(".".repeat(num));
-
-// f(20);
 const splitString = (string, len = 10) => {
     let resArr = [];
     let startIndex = 0;
@@ -42,9 +14,10 @@ const splitString = (string, len = 10) => {
             break;
         }
         // пушу массив и если нужны точки - добавляю  
-        resArr.push(chunk[0].toUpperCase() + chunk.slice(1) + ".".repeat(len - chunk.length));
+        resArr.push(chunk + ".".repeat(len - chunk.length));
         startIndex += len;
     }
     return resArr;
 };
 console.log(splitString(myString, 3));
+
