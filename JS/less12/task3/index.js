9430412ff832ee36d3646e73ec33a74b8a978563
unknown <shotvideo25@gmail.com>
2020-05-25 16:06:43 +0300

    Nan

--- JS/less12/task3/index.js
+++ JS/less12/task3/index.js
@@ -1,10 +1,11 @@
 
 
 const cleanTransactionsList = arrSum => {
-    return arrSum.map(sum => '$' + parseFloat(sum).toFixed(2));
+    return arrSum.map(sum => '$' + parseFloat(sum).toFixed(2))
+        .filter(item => !item.includes('NaN'));
 };
 
-const a = [' 345 ', '  -12 dollars', '13.6', '1.7', '1qwe'];
+const a = [' 345 ', '  -12 dollars', '13.6', '1.7', '1qwe', 'wqeqwe', ' ', undefined, null, true];
  
 console.log(cleanTransactionsList(a));
 console.log(a);
