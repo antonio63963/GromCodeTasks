2b81e83a59688bdc5588e6e219a2c8408140dbb0
unknown <shotvideo25@gmail.com>
2020-05-25 16:43:43 +0300

    to number

--- JS/less12/task3/index.js
+++ JS/less12/task3/index.js
@@ -1,12 +1,19 @@
 
 
+// const cleanTransactionsList = arrSum => {
+//     return arrSum.map(sum => parseFloat(sum))
+//         .filter(item => !Number.isNaN(item))
+//         .map(item => '$' + item.toFixed(2));
+// };
+
+
 const cleanTransactionsList = arrSum => {
-    return arrSum.map(sum => parseFloat(sum))
-        .filter(item => !Number.isNaN(item))
-        .map(item => '$' + item.toFixed(2));
+    return arrSum
+        .filter(item => Number(item) && typeof item !== 'boolean')
+        .map(item => '$' + Number(item).toFixed(2));
 };
 
-const a = [' 345 ', '  -12 dollars', '13.6', '1.7', '1qwe', 'wqeqwe', ' ', undefined, null, true];
+const a = [' 345 ', '  -12 dollars', '13.623', '1.7', '1qwe', 'wqeqwe', '', undefined, null, true];
  
 console.log(cleanTransactionsList(a));
 console.log(a);
