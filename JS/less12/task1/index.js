53dad88ee28e395a1ab2b5527c52b3d7ce024996
unknown <shotvideo25@gmail.com>
2020-05-25 15:08:44 +0300

    calc

---
+++ JS/less12/task1/index.js
@@ -0,0 +1,24 @@
+let a = '3 + 4';
+
+const calc = expression => {
+    const [a, operator, b] = expression.split(' ');
+    let result;
+
+    switch (operator) {
+        case '+':
+            result = Number(a) + Number(b);
+            break;
+        case '-':
+            result = a - b;
+            break;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            result = a / b;
+            break;
+    }
+    return result;
+};
+
+console.log(calc('8 / 2'));
