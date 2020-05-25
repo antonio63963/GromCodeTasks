5b16fb0c37249090fb2c6aee14bbca835633d955
unknown <shotvideo25@gmail.com>
2020-05-25 16:01:58 +0300

    toFixed

--- JS/less12/task3/index.js
+++ JS/less12/task3/index.js
@@ -1,9 +1,10 @@
 
 
 const cleanTransactionsList = arrSum => {
-    return arrSum.map(sum => parseFloat(sum));
+    return arrSum.map(sum => '$' + parseFloat(sum).toFixed(2));
 };
 
-const a = [' 345 ', '  -12 dollars', '13.6', '1.7'];
+const a = [' 345 ', '  -12 dollars', '13.6', '1.7', '1qwe'];
  
-console.log(cleanTransactionsList(a).join(','));
+console.log(cleanTransactionsList(a));
+console.log(a);
