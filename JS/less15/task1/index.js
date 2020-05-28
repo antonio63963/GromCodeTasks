3326df1a17561c573ddd948f25d423ebd80f9747
unknown <shotvideo25@gmail.com>
2020-05-28 19:15:54 +0300

    cunter right

--- JS/less15/task1/index.js
+++ JS/less15/task1/index.js
@@ -1,12 +1,19 @@
-'use strict'; 
+'use strict';
 
 
 
 export function makeCounter() {
     let count = 0;
-    return function() {
-        count++;
+    return function () {
+        return count++;
     };
 };
 
+// let counter1 = makeCounter();
+// let counter2 = makeCounter();
 
+// console.log(counter1());
+// console.log(counter1());
+// console.log(counter1());
+
+// console.log(counter2());
