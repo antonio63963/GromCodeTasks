254004669376091acf3066bb36d3ccf0a23089c6
unknown <shotvideo25@gmail.com>
2020-05-30 17:54:43 +0300

    done

--- JS/less16/task3/index.js
+++ JS/less16/task3/index.js
@@ -1,22 +1,21 @@
 'use strict';
 
-export function createArrayOfFunctions(len) {
-    if (typeof len !== 'number') {
-        return null;
+function createArrayOfFunctions(len) {
+    let arr = [];
+    if (typeof len == 'number') {
+        for (let i = 0; i < len; i++) {
+            arr.push(function () {
+                return i;
+            });
+        }
     } else if (typeof len == 'undefined') {
         return [];
+    } else {
+        return null;
     }
-    let arr = [];
-
-    for (let i = 0; i < len; i++) {
-        arr.push(function () {
-            return i;
-        });
-    }
-
     return arr;
 };
 
 console.log(createArrayOfFunctions('5'));
-console.log(createArrayOfFunctions(5));
+console.log(createArrayOfFunctions());
 console.log(createArrayOfFunctions(10)[5]());
