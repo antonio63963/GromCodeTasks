f595e92e504c27467e642a0c7d8c452a9a120a7c
unknown <shotvideo25@gmail.com>
2020-05-30 17:42:40 +0300

    isNan

--- JS/less16/task3/index.js
+++ JS/less16/task3/index.js
@@ -1,17 +1,21 @@
 'use strict';
 
-export function createArrayOfFunctions(len) {
-    if(typeof len == NaN) { return null;}
-    else if(typeof len == 'undefined') { return [];}
+function createArrayOfFunctions(len) {
+    if (!Number.isNaN(len)) {
+        return null;
+    } else if (typeof len == 'undefined') {
+        return [];
+    }
     let arr = [];
 
-    for(let i = 0; i < len; i++) {
-        arr.push(function() { return i;});
+    for (let i = 0; i < len; i++) {
+        arr.push(function () {
+            return i;
+        });
     }
- 
+
     return arr;
 };
 
-console.log(createArrayOfFunctions(10));
+console.log(createArrayOfFunctions('5'));
 console.log(createArrayOfFunctions(10)[5]());
-
