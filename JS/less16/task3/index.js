d6df7e6ffa9acd1f3f5ee433bd15207ce9727f05
unknown <shotvideo25@gmail.com>
2020-05-30 17:55:00 +0300

    ex

--- JS/less16/task3/index.js
+++ JS/less16/task3/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function createArrayOfFunctions(len) {
+export function createArrayOfFunctions(len) {
     let arr = [];
     if (typeof len == 'number') {
         for (let i = 0; i < len; i++) {
