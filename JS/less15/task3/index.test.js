4b2d1f629b7485af21cf60aa005016f3c7843a6d
unknown <shotvideo25@gmail.com>
2020-05-30 14:00:14 +0300

    right tests by jest

--- JS/less15/task3/index.test.js
+++ JS/less15/task3/index.test.js
@@ -5,9 +5,9 @@ it(" should get arr of obj", () => {
     logger.warn('it have works!');
     logger.error("it's mistake");
     logger.log("move on");
-    expect(logger.getRecords()).toEqual([{massage: 'move on', dateTime: new Date(),
-    type: 'log'},  {massage: "it's mistake", dateTime: new Date(),
-    type: 'error'}, {massage: 'it have works!', dateTime: new Date(),
+    expect(logger.getRecords()).toEqual([{message: 'move on', dateTime: new Date(),
+    type: 'log'},  {message: "it's mistake", dateTime: new Date(),
+    type: 'error'}, {message: 'it have works!', dateTime: new Date(),
     type: 'warn'}]);
 });
 
@@ -16,7 +16,7 @@ it(" should get arr with only selected obj 'warn' ", () => {
     logger.warn('it have works!');
     logger.error("it's mistake");
     logger.log("move on");
-    expect(logger.getRecords('warn')).toEqual([{massage: 'it have works!', dateTime: new Date(),
+    expect(logger.getRecords('warn')).toEqual([{message: 'it have works!', dateTime: new Date(),
     type: 'warn'}]);
 });
 
