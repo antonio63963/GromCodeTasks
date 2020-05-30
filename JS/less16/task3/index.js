4bde192a7ad23263e87ca6dd20fcda31dfe69953
unknown <shotvideo25@gmail.com>
2020-05-30 17:34:25 +0300

    return arr functions

---
+++ JS/less16/task3/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const createArrayOfFunctions = (len) => {
+    let arr = [];
+
+    for(let i = 0; i < len; i++) {
+        arr.push(function() { return i;});
+    }
+ 
+    return arr;
+};
+
+console.log(createArrayOfFunctions(10));
+console.log(createArrayOfFunctions(10)[5]());
