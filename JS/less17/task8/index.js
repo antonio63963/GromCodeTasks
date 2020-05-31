c9981e8790fca4398f6ab58dce896a05b0f10368
unknown <shotvideo25@gmail.com>
2020-05-31 19:59:29 +0300

    <=60

--- JS/less17/task8/index.js
+++ JS/less17/task8/index.js
@@ -1,9 +1,9 @@
-export const timer = {
+const timer = {
     secondsPassed: 0,
     minsPassed: 0,
     startTimer() {
         setInterval(() => {
-            if (this.secondsPassed < 60) {
+            if (this.secondsPassed <= 60) {
                 this.secondsPassed += 1;
             } else {
                 this.secondsPassed = 0;
@@ -31,3 +31,19 @@ export const timer = {
 timer.startTimer();
 console.log(timer.getTime());
 console.log(timer.getTime());
+
+let secondsPassed = 0;
+let minsPassed = 0;
+function startTimer() {
+    setInterval(() => {
+        if (secondsPassed < 60) {
+            secondsPassed += 1;
+        } else {
+            secondsPassed = 0;
+            minsPassed += 1;
+        }
+        console.log(`${minsPassed} : ${secondsPassed}`);
+    }, 1000);
+}
+startTimer();
+
