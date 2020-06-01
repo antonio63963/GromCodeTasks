1c35f26e67f636a3b9cc5e0c2de3c8a29692fbb0
unknown <shotvideo25@gmail.com>
2020-06-01 13:00:38 +0300

    sdfsdf

--- JS/less17/task10/index.js
+++ JS/less17/task10/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-export function bind(func, context) {
-    return func.call(context, ...[arguments]);
+export function bind(func, context, arg1, arg2, [argn]) {
+    return func.call(context, arg1, arg2, ...[argn]);
 
 }
