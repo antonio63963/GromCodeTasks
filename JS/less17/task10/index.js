dec4b38c1db98bd965aa0aea6ac38e0afafa9af4
unknown <shotvideo25@gmail.com>
2020-06-01 14:39:10 +0300

    hz

--- JS/less17/task10/index.js
+++ JS/less17/task10/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-export function bind(func, context, arg1, arg2) {
-    const newFunc = func();
-    return newFunc.call(context, arg1, arg2);
+export function bind(func, context) {
+    const newFunc = func.call(context, ...arguments);
+    return newFunc;
 
 }
