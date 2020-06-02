b4ff5dc6b030b6388b7945517ae74ae9fec8bb85
unknown <shotvideo25@gmail.com>
2020-06-02 14:54:58 +0300

    for

--- JS/less17/task10/index.js
+++ JS/less17/task10/index.js
@@ -2,15 +2,23 @@
 
 export function bind(func, context) {
      
-     const arg = arguments;
-    return function() {return func.apply(context, arg);};
+    let arg = [];
+    for(let i = 2; i < arguments.length; i++) {
+       arg.push(arguments[i]);
+    }
+   
+    console.log(arg);
+    return function() {
+       
+        return func.apply(context, arg);
+    };
 }
-// const obj = {
-//     name: 'Denny',
-//     gritting(make, len) {
-//         console.log(`Hi, I'm ${this.name} i ${make} my brain by ${len}`);
-//     }
-// };
+const obj = {
+    name: 'Denny',
+    gritting(make, len) {
+        console.log(`Hi, I'm ${this.name} i ${make} my brain by ${len}`);
+    }
+};
 
-// const a = bind(obj.gritting, {name: 'Frank'});
-// a('split', 'JS');
+const a = bind(obj.gritting, {name: 'Frank'}, 'split', 'JS');
+a();
