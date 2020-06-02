ac24ce96ba24a904a7bb8bb8d410524c2a2daca1
unknown <shotvideo25@gmail.com>
2020-06-02 19:28:24 +0300

    sum

--- JS/less17/task10/index.js
+++ JS/less17/task10/index.js
@@ -1,34 +1,34 @@
 
-export function bind(func, context) {
-    // обрезаем ненужные аргументы (функцию и контекст)
-   let bindArgs = [].slice.call(arguments, 2);
+// export function bind(func, context) {
+//     // обрезаем ненужные аргументы (функцию и контекст)
+//    let bindArgs = [].slice.call(arguments, 2);
  
-    return function() {
-      // здесь все аргументы будут необходимы
-      let fnArgs = [].slice.call(arguments);
-   
-      // собираем все 
-      return func.apply(context, bindArgs.concat(fnArgs));
-    };
-  };
-
-
-//  function bind(func, context) {
-     
-//     let arg = [];
-//     for(let i = 2; i < arguments.length; i++) {
-//        arg.push(arguments[i]);
-//     }
-   
-//     console.log(arg);
 //     return function() {
-       
-//         return func.apply(context, arg);
+//       // здесь все аргументы будут необходимы
+//       let fnArgs = [].slice.call(arguments);
+   
+//       // собираем все 
+//       return func.apply(context, bindArgs.concat(fnArgs));
 //     };
-// }
+//   };
+
+
+ function bind(func, context) {
+     
+    let arg = [];
+    for(let i = 2; i < arguments.length; i++) {
+       arg.push(arguments[i]);
+    }
+   
+    console.log(arg);
+    return function() {
+       
+        return func.apply(context, arg);
+    };
+}
 const obj = {
     name: 'Denny',
-    gritting(a, b) {
+    sum(a, b) {
         const c = a + b;
         console.log(`${this.name}  made a new function  ${c}`);
     }
@@ -39,11 +39,11 @@ const obj2 = {
 };
 
 
-const a = bind(obj.gritting, {name: 'Frank'}, 2, 8);
+const a = bind(obj.sum, {name: 'Frank'}, 2, 8);
 a();
 a.call(obj2);
 a.bind({name: 'Misha'}, 'saddad', 'dfsfsf');
-const b = obj.gritting.bind({name: 'Misha'}, 'saddad', 'dfsfsf');
+const b = obj.sum.bind({name: 'Misha'}, 3, 9);
 b();
 b.bind({ name: 'Sasha'});
 b();
