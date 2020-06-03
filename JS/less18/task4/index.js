f94b2b908358723e4f33bb3fa5d0078cc5dada3c
unknown <shotvideo25@gmail.com>
2020-06-03 21:21:44 +0300

    this

--- JS/less18/task4/index.js
+++ JS/less18/task4/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
 
-export function saveCalls(fn) {
+ function saveCalls(fn) {
 
     let calls = [];
     function withMemory() {
         calls.push([...arguments]);
-        return fn.call(null, arguments);
+        return  fn.call(this, arguments);
     }
     withMemory.calls = calls;
     return withMemory;
@@ -15,8 +15,18 @@ export function saveCalls(fn) {
 function test(a, b) {
     return a + b;
 }
+const user = {
+    name:"John",
+    sayHi() {
+        return this.name;
+    }
+};
 
 const testWithMemory = saveCalls(test);
 testWithMemory(4, 5);
 testWithMemory(10, 3);
 console.log(testWithMemory.calls);
+
+const methodWithMemory = saveCalls(user.sayHi);
+methodWithMemory.call({name: 'Bob'}, 23);
+console.log(methodWithMemory.calls);
