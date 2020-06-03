d8d46a876811dcfc53bb24af0ed2f5934258be3e
unknown <shotvideo25@gmail.com>
2020-06-03 15:37:52 +0300

    pseudo arr + reduce

---
+++ JS/less18/task3/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function sumOfSquares() {
+    
+    return [...arguments].reduce((acc, elem) => {
+        return acc + elem**2;
+    }, 0);
+}
+
+
+console.log(sumOfSquares(1, 2, 3));
