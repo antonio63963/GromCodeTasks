a0d7b9186c26fafabcd9f2dc7f25e0182d845a38
unknown <shotvideo25@gmail.com>
2020-06-04 14:22:07 +0300

    proto set out the function

--- JS/less19/task3/index.js
+++ JS/less19/task3/index.js
@@ -3,22 +3,22 @@
 export function User(name, age) {
     this.name = name;
     this.age = age;
-    User.prototype.sayHi() {
-        console.log(`Hi, I'am ${this.name}`);
-    }
-    User.prototype.requestNewPhoto() {
-        console.log(`New photo request was sent for ${this.name}`);
-    }
-    User.prototype.setAge(value) {
-        if (value < 0) {
-            return false;
-        } else {
-            this.age = value;
-            if (this.age >= 25) {
-                this.requestNewPhoto();
-            }
-            return this.age;
+}
+User.prototype.sayHi() {
+    console.log(`Hi, I'am ${this.name}`);
+}
+User.prototype.requestNewPhoto() {
+    console.log(`New photo request was sent for ${this.name}`);
+}
+User.prototype.setAge(value) {
+    if (value < 0) {
+        return false;
+    } else {
+        this.age = value;
+        if (this.age >= 25) {
+            this.requestNewPhoto();
         }
-
+        return this.age;
     }
+
 }
