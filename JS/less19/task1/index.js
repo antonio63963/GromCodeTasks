3922b92a3b380c1e3a9f01ce65f164e00bd8ff34
unknown <shotvideo25@gmail.com>
2020-06-04 13:52:16 +0300

    stop into native method

--- JS/less19/task1/index.js
+++ JS/less19/task1/index.js
@@ -11,12 +11,14 @@ export const vehicle = {
 };
 
 export const ship = {
-    name: "Argo",
+    
     startMachine() {
         console.log(`${this.name} lifting anchor up`);
+        this.move();
     },
     stopMachine() {
         console.log(`${this.name} lifting anchor down`);
+        this.stop();
     },
     __proto__: vehicle
 };
