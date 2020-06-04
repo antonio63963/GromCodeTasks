6f38c508c2d0f9673a9deeee009c5f65f1f89b58
unknown <shotvideo25@gmail.com>
2020-06-04 14:38:46 +0300

    gettr setter

---
+++ JS/less19/task4/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const user = {
+    firstName: 'Mik',
+    lastName: 'Jagger',
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    },
+    set fullName(value) {
+        const arr = value.split(' ');
+        this.firstName = arr[0];
+        this.lastName = arr[1];
+    },
+};
+
+console.log(user.fullName);
+user.fullName ='James Bond';
+console.log(user.fullName);
