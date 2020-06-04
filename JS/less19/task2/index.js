53fac8331a08e5fc35b2e7713c63007d96300bd4
unknown <shotvideo25@gmail.com>
2020-06-04 13:46:29 +0300

    not a function

--- JS/less19/task2/index.js
+++ JS/less19/task2/index.js
@@ -1,11 +1,19 @@
 'use strict';
 
-export function getOwnProps(obj) {
-    let arr = [];
-    for (let prop in obj) {
-        if (obj.hasOwnProperty(prop)) {
-            arr.push(prop);
-        }
-    }
-    return arr;
+function getOwnProps(obj) {
+    return Object.entries(obj)
+        .filter(item => typeof item[1] !== 'function')
+        .map(item => item[0]);
+
+
 }
+
+const user = {
+    name: "Joe",
+    sirname: "baiden",
+    sayHi() {
+        console.log(`${this.name} are your big frend, Petya!`);
+    }
+};
+
+console.log(getOwnProps(user));
