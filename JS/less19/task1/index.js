56db1f4dae6a102a614a4c6814735ce581f7df0a
unknown <shotvideo25@gmail.com>
2020-06-04 11:53:34 +0300

    anchor up!

---
+++ JS/less19/task1/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const vehicle = {
+    name: "Argo",
+    move() {
+        console.log(`${this.name} is moving`);
+    },
+    stop() {
+        console.log(`${this.name} stopped`);
+    }
+};
+
+const ship = {
+    stopMachine() {
+        console.log(`${this.name} lifting anchor down`);
+    },
+    startMachine() {
+        console.log(`${this.name} lifting anchor up`);
+    }
+    
+};
+Object.setPrototypeOf(ship, vehicle);
+
+ship.startMachine();
+ship.move();
+ship.stop();
+ship.stopMachine();
+
+
+
+
+
+
