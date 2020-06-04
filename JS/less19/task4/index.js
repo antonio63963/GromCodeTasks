bc1b9c725129308d73eddde63dd2a8e77450f400
unknown <shotvideo25@gmail.com>
2020-06-04 14:53:49 +0300

    destruct

--- JS/less19/task4/index.js
+++ JS/less19/task4/index.js
@@ -7,9 +7,9 @@ export default {
         return `${this.firstName} ${this.lastName}`;
     },
     set fullName(value) {
-        const arr = value.split(' ');
-        this.firstName = arr[0];
-        this.lastName = arr[1];
+        const [firstName, lastName] = value.split(' ');
+        this.firstName = firstName;
+        this.lastName = lastName;
     },
 };
 
