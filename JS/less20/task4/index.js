11a81cbd3668c17448b2eb12c061bf1484a21653
unknown <shotvideo25@gmail.com>
2020-06-06 17:49:26 +0300

    order

---
+++ JS/less20/task4/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+class Order {
+    constructor(price, city, type) {
+        this.id = Math.floor(Math.random(100) * 100);
+        this.price = price;
+        this.dataCreated = new Date();
+        this.isConfirmed = false;
+        this.dateConfirmed;
+        this.city = city;
+        this.type = type;
+    }
+
+    checkPrice() {
+        return this.price > 1000 ? true : false;
+    }
+
+    confirmOrder() {
+        if (this.isConfirmed == false) {
+            this.isConfirmed = true;
+            this.dateConfirmed = new Date();
+        }
+    }
+
+    isValidType(type) {
+        return type == 'Buy' || type == 'Sell' ? 
+        true :
+        false;
+
+    }
+}
