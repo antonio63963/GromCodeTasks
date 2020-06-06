24891fbbd747a0860536b621afa3e81890df716c
unknown <shotvideo25@gmail.com>
2020-06-06 12:29:07 +0300

    class

---
+++ JS/less20/task1/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+class User {
+    constructor (name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    sayHi() {
+        console.log(`Hi, I am ${this.name} `);
+    }
+
+    requestNewPhoto() {
+        console.log(`New photo request was sent for ${this.name}`);
+    }
+
+    setAge(value) {
+        if(value < 0) {
+            return false;
+        } else {
+            return this.age;
+        }
+    } 
+
+    static createEmpty() {
+        this.name = '';
+        this.age = null;
+    }
+}
+
+
+
+
+
+
+
