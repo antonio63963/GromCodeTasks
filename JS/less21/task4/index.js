8e494847575ed2e26adc2649ce2040c7a4c95b40
unknown <shotvideo25@gmail.com>
2020-06-08 14:28:31 +0300

    found elements

---
+++ JS/less21/task4/index.js
@@ -0,0 +1,24 @@
+export function getTitle() {
+    const title = document.querySelector('.title');
+    return title.textContent;
+}
+
+export function getDescription() {
+    const elem = document.querySelector('.about');
+    return elem.innerText;
+}
+
+export function getPlans() {
+    const elem = document.querySelector('.plans');
+    return elem.innerHTML;
+}
+
+export function getGoal() {
+    const elem = document.querySelector('.goal');
+    return elem.outerHTML;
+}
+
+console.log(getTitle());
+console.log(getDescription());
+console.log(getPlans());
+console.log(getGoal());
