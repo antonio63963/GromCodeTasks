d8cbe162aca8b70d96d2e49daa3572c73683d873
unknown <shotvideo25@gmail.com>
2020-06-08 13:35:09 +0300

    dfsfsdf

---
+++ JS/less21/task2/index.js
@@ -0,0 +1,16 @@
+
+
+function getTitleElement() {
+    const titleElem = document.querySelector('.title');
+    console.dir(titleElem);
+    return titleElem;
+}
+
+const getInputElement = () => {
+    const collection =  [...document.querySelectorAll('input')];
+    const result = collection.filter(item => item.type == 'text');
+    console.log(...result);
+};
+
+getTitleElement();
+getInputElement();
