9265836a04580bac9638d16c5222074b3dbb0570
unknown <shotvideo25@gmail.com>
2020-06-08 12:45:56 +0300

    dom first

---
+++ JS/less21/index.js
@@ -0,0 +1,50 @@
+const tasks = [{
+        text: 'Buy milk',
+        done: false
+    },
+    {
+        text: 'Pick up Tom from airport',
+        done: false
+    },
+    {
+        text: 'Visit party',
+        done: false
+    },
+    {
+        text: 'Visit doctor',
+        done: true
+    },
+    {
+        text: 'Buy meat',
+        done: true
+    },
+];
+
+const tasksList = document.querySelector('.list');
+const addTaskEllements = (arr) => {
+    const tasksArr = arr.sort((a, b) => a.done - b.done)
+        .map(({
+            text,
+            done
+        }) => {
+            let taskItem = document.createElement('li');
+            taskItem.classList.add('list__item');
+            let checkbox = document.createElement('input');
+            checkbox.classList.add('list__item-checkbox');
+            checkbox.setAttribute('type', 'checkbox');
+            if (done) {
+                checkbox.setAttribute('checked', done);
+                taskItem.classList.add('list__item_done');
+            }
+            taskItem.append(checkbox);
+            taskItem.append(text);
+            return taskItem;
+        });
+
+    return tasksArr;
+};
+
+
+const tasksArr = addTaskEllements(tasks);
+tasksList.append(...tasksArr);
+const htmlTasks = [...tasksList.innerHTML].sort((a, b) => a.checked - b.checked);
