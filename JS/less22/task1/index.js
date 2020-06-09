5067e3d1a0d856d31287360feb31f59eff6709ab
unknown <shotvideo25@gmail.com>
2020-06-09 15:12:43 +0300

    bubles $ capture

--- JS/less22/task1/index.js
+++ JS/less22/task1/index.js
@@ -17,28 +17,45 @@ const clearList = () => {
 };
 
 const divHandler = () => {
-    event.stopPropagation();
+    // event.stopPropagation();
     inputColorText('grey', 'DIV');
 };
+const divHandler2 = () => {
+     inputColorText('green', 'DIV');
+};
+
 const pHandler = () => {
-    event.stopPropagation();
+    // event.stopPropagation();
     inputColorText('grey', 'P');
 };
+const pHandler2 = () => {
+       inputColorText('green', 'P');
+};
+
 const spanHandler = () => {
-    event.stopPropagation();
+    // event.stopPropagation();
     inputColorText('grey', 'SPAN');
+};
+const spanHandler2 = () => {
+    inputColorText('green', 'SPAN');
 };
 
 const removeHandler = () => {
     divElem.removeEventListener('click', divHandler);
+    divElem.removeEventListener('click', divHandler2);
     pElem.removeEventListener('click', pHandler);
+    pElem.removeEventListener('click', pHandler2);
     spanElem.removeEventListener('click', spanHandler);
+    spanElem.removeEventListener('click', spanHandler2);
 };
 
 const attachHandlers = () => {
     divElem.addEventListener('click', divHandler);
+    divElem.addEventListener('click', divHandler2, true);
     pElem.addEventListener('click', pHandler);
+    pElem.addEventListener('click', pHandler2, true);
     spanElem.addEventListener('click', spanHandler);
+    spanElem.addEventListener('click', spanHandler2, true);
 };
 
 
@@ -47,5 +64,8 @@ removeHandlerBtn.addEventListener('click', () => removeHandler());
 attachHandlersBtn.addEventListener('click', () => attachHandlers());
 
 divElem.addEventListener('click', divHandler);
+divElem.addEventListener('click', divHandler2, true);
 pElem.addEventListener('click', pHandler);
+pElem.addEventListener('click', pHandler2, true);
 spanElem.addEventListener('click', spanHandler);
+spanElem.addEventListener('click', spanHandler2, true);
