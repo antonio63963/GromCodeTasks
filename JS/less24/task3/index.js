97a269aecb0383676c84eab91cf15d46ed5d470f
unknown <shotvideo25@gmail.com>
2020-06-14 23:21:43 +0300

    hz

--- JS/less24/task3/index.js
+++ JS/less24/task3/index.js
@@ -1,21 +1,21 @@
-const howManyDays = (millisec) => {
-    let timeLeft = millisec;
-    const day = 1000 * 3600 * 24;
-    const hour = 1000 * 3600;
-    const minute = 1000 * 60;
-    const sec = 1000;
+// const howManyDays = (millisec) => {
+//     let timeLeft = millisec;
+//     const day = 1000 * 3600 * 24;
+//     const hour = 1000 * 3600;
+//     const minute = 1000 * 60;
+//     const sec = 1000;
 
-    const days = Math.trunc(timeLeft / day);
-    timeLeft = timeLeft % day;
-    const hours = Math.trunc(timeLeft / hour);
-    timeLeft = timeLeft % hour;
-    const minutes = Math.trunc(timeLeft / minute);
-    timeLeft = timeLeft % minute;
-    const seconds = timeLeft / sec;
+//     const days = Math.trunc(timeLeft / day);
+//     timeLeft = timeLeft % day;
+//     const hours = Math.trunc(timeLeft / hour);
+//     timeLeft = timeLeft % hour;
+//     const minutes = Math.trunc(timeLeft / minute);
+//     timeLeft = timeLeft % minute;
+//     const seconds = timeLeft / sec;
 
-    console.log(`${days}d ${hours}h ${minutes}m ${seconds}s`);
+//     console.log(`${days}d ${hours}h ${minutes}m ${seconds}s`);
 
-}
+// }
 
 export const getDiff = (startDate, endDate) => {
 
@@ -24,7 +24,24 @@ export const getDiff = (startDate, endDate) => {
     const end = new Date(endDate).getTime();
     const resultMillisec = Math.abs(end - start);
     const millisecDiff = new Date(resultMillisec);
-
+    const howManyDays = (millisec) => {
+        let timeLeft = millisec;
+        const day = 1000 * 3600 * 24;
+        const hour = 1000 * 3600;
+        const minute = 1000 * 60;
+        const sec = 1000;
+    
+        const days = Math.trunc(timeLeft / day);
+        timeLeft = timeLeft % day;
+        const hours = Math.trunc(timeLeft / hour);
+        timeLeft = timeLeft % hour;
+        const minutes = Math.trunc(timeLeft / minute);
+        timeLeft = timeLeft % minute;
+        const seconds = timeLeft / sec;
+    
+        console.log(`${days}d ${hours}h ${minutes}m ${seconds}s`);
+    
+    };
    return howManyDays(millisecDiff);
 
 };
