1264aa228d5f2ba7eea50d69fd09fb272a9ff2c3
unknown <shotvideo25@gmail.com>
2020-06-14 22:58:41 +0300

    get difference between two dates

--- JS/less24/task3/index.js
+++ JS/less24/task3/index.js
@@ -1,5 +1,31 @@
-const howManyDays = (millisec) => {
-    let timeLeft = millisec;
+// const howManyDays = (millisec) => {
+//     let timeLeft = millisec;
+//     const day = 1000 * 3600 * 24;
+//     const hour = 1000 * 3600;
+//     const minute = 1000 * 60;
+//     const sec = 1000;
+
+//     const days = Math.trunc(timeLeft / day);
+//     timeLeft = timeLeft % day;
+//     const hours = Math.trunc(timeLeft / hour);
+//     timeLeft = timeLeft % hour;
+//     const minutes = Math.trunc(timeLeft / minute);
+//     timeLeft = timeLeft % minute;
+//     const seconds = timeLeft / 1000;
+
+//     console.log(`${days}d ${hours}h ${minutes}m ${seconds}s`);
+
+// }
+
+ const getDiff = (startDate, endDate) => {
+
+
+    const start = new Date(startDate).getTime();
+    const end = new Date(endDate).getTime();
+    const resultMillisec = Math.abs(end - start);
+    const millisecDiff = new Date(resultMillisec);
+
+    let timeLeft = millisecDiff;
     const day = 1000 * 3600 * 24;
     const hour = 1000 * 3600;
     const minute = 1000 * 60;
@@ -14,18 +40,7 @@ const howManyDays = (millisec) => {
     const seconds = timeLeft / 1000;
 
     console.log(`${days}d ${hours}h ${minutes}m ${seconds}s`);
-
-}
-
-export const getDiff = (startDate, endDate) => {
-
-
-    const start = new Date(startDate).getTime();
-    const end = new Date(endDate).getTime();
-    const resultMillisec = Math.abs(end - start);
-    const millisecDiff = new Date(resultMillisec);
-
-   return howManyDays(millisecDiff);
+  
 
 };
 
