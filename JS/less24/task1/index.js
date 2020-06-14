a39a33af33c3c3d9b34df3f746f6573c5fae1a3a
unknown <shotvideo25@gmail.com>
2020-06-14 17:04:43 +0300

    future day

---
+++ JS/less24/task1/index.js
@@ -0,0 +1,12 @@
+
+
+export const dayOfWeek = (date, days) => {
+     const daysArr = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
+     const day = new Date(date).getDate();
+     const futureDayMillisec = new Date(date).setDate(day + days);
+     const futureDay = new Date(futureDayMillisec);
+
+     console.log(daysArr[futureDay.getDay()]);
+     return daysArr[futureDay];
+ };
+
