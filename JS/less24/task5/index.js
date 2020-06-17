11fe54b96e2b09fce36166dea6b81e07a9234ef3
unknown <shotvideo25@gmail.com>
2020-06-17 14:41:03 +0300

    dfasdfsdf

--- JS/less24/task5/index.js
+++ JS/less24/task5/index.js
@@ -10,7 +10,10 @@ const students = [{
         name: 'Sam',
         birthday: '03/15/2010'
     },
-    
+    {
+        name: 'Jack',
+        birthday: '03/25/2001'
+    },
    
 ];
 
@@ -47,17 +50,32 @@ export const studentsBirthDays = (students) => {
 
  }
 
-// studentsBirthDays(students);
-// console.log(studentsBirthDays(students));
+console.log(studentsBirthDays(students));
 
-// const birth = {
-//     Jan: ['Tom'],
-//     Feb : ['Jack', 'Bob']
-// };
 
-// for(let key in birth) {
-//     birth[key].push('gggggg');
-// }
-// const h = Object.entries(birth);
-// // birth.Feb.push('rrrrr')
-// console.log(h);
+// export const studentsBirthDays = (students) => {
+//     const copyArr = [...students];
+//     let monthesArr = ['null', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+//     let sortedByBirthday =[];
+  
+//     const gradationByBirthday = {};
+
+//     copyArr.forEach((student) => {
+//         const dateArr = student.birthday.split('/');
+//         monthesArr.forEach((month, index) => {
+//             if(index == dateArr[0]) {
+//             sortedByBirthday.push([month, dateArr[1]]);
+//                 month[1].sort((a, b) => b[1] - a[1]);
+//             }
+//         });
+//     });
+//     // console.log(monthesArr);
+//     monthesArr.forEach(elem => {
+//             if(elem[1].length > 0 &&  elem !== 'null') {
+//                 gradationByBirthday[elem[0]] = elem[1].map(name => name[0]);
+//             }
+//     })
+//     return gradationByBirthday;
+   
+
+//  }
