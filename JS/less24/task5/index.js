c4b88bfc4769ebc03275c8a7ef5c9f81f7786b8e
unknown <shotvideo25@gmail.com>
2020-06-18 18:34:01 +0300

    birthdays students

--- JS/less24/task5/index.js
+++ JS/less24/task5/index.js
@@ -14,57 +14,21 @@ const students = [{
         name: 'Jack',
         birthday: '03/25/2001'
     },
-   
+
 ];
-
- /*
- 0. создаю массив месяцов
- 1. сплитом разбиваю дату 
- 2. определяю месяц 
- 3. создаю результирующий объект
-   
-*/
-export const studentsBirthDays = (students) => {
-    const copyArr = [...students];
-    let monthesArr = [['null', []], ['Jan', []], ['Feb', []], ['Mar', []], ['Apr', []], ['May', []], ['Jun', []], ['Jul', []], ['Aug', []], ['Sept', []], ['Oct', []], ['Nov', []], ['Dec', []]];
-  
-    const gradationByBirthday = {};
-
-    copyArr.forEach((student) => {
-        const dateArr = student.birthday.split('/');
-        monthesArr.forEach((month, index) => {
-            if(index == dateArr[0]) {
-            month[1].push([student.name, dateArr[1]]);
-                month[1].sort((a, b) => b[1] - a[1]);
-            }
-        });
-    });
-    // console.log(monthesArr);
-    monthesArr.forEach(elem => {
-            if(elem[1].length > 0 &&  elem !== 'null') {
-                gradationByBirthday[elem[0]] = elem[1].map(name => name[0]);
-            }
-    })
-    return gradationByBirthday;
-   
-
- }
-
-console.log(studentsBirthDays(students));
 
 
 // export const studentsBirthDays = (students) => {
 //     const copyArr = [...students];
-//     let monthesArr = ['null', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
-//     let sortedByBirthday =[];
-  
+//     let monthesArr = [['null', []], ['Jan', []], ['Feb', []], ['Mar', []], ['Apr', []], ['May', []], ['Jun', []], ['Jul', []], ['Aug', []], ['Sept', []], ['Oct', []], ['Nov', []], ['Dec', []]];
+
 //     const gradationByBirthday = {};
 
 //     copyArr.forEach((student) => {
 //         const dateArr = student.birthday.split('/');
 //         monthesArr.forEach((month, index) => {
 //             if(index == dateArr[0]) {
-//             sortedByBirthday.push([month, dateArr[1]]);
+//             month[1].push([student.name, dateArr[1]]);
 //                 month[1].sort((a, b) => b[1] - a[1]);
 //             }
 //         });
@@ -74,8 +38,47 @@ console.log(studentsBirthDays(students));
 //             if(elem[1].length > 0 &&  elem !== 'null') {
 //                 gradationByBirthday[elem[0]] = elem[1].map(name => name[0]);
 //             }
-//     })
+//     });
 //     return gradationByBirthday;
-   
+
 
 //  }
+
+// console.log(studentsBirthDays(students));
+
+
+const sorting = (a, b) => {
+    if (b.birthday.getDate() - a.birthday.getDate() !== 0) {
+        b.birthday.getDate() - a.birthday.getDate();
+    }
+    if (a.birthday.getDate() - b.birthday.getDate()) {
+        return a.birthday.getMonth() - b.birthday.getMonth();
+    }
+}
+
+export const studentsBirthDays = (students) => {
+    const copyArr = [...students];
+    let birthdayList = {};
+    const formatedDateArr = copyArr.map((student) => {
+        student.birthday = new Date(student.birthday);
+        return student;
+    }).sort(sorting);
+    formatedDateArr.forEach(student => {
+        student.birthday = student.birthday.toDateString()
+            .split(' ')
+            .splice(1, 3);
+        const month = student.birthday[0];
+
+        if (Object.keys(birthdayList).includes(month)) {
+            birthdayList[month].push(student.name);
+        } else {
+            birthdayList[month] = [student.name];
+        }
+
+    });
+    console.log(birthdayList);
+    return {birthdayList};
+};
+
+studentsBirthDays(students);
+
