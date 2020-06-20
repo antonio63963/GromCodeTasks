67515b4f2fb0d223870b4210c821e3e9fec299bd
unknown <shotvideo25@gmail.com>
2020-06-20 11:16:49 +0300

    students birthdays

--- JS/less24/task5/index.js
+++ JS/less24/task5/index.js
@@ -30,67 +30,24 @@ const students = [{
 ];
 
 
-// export const studentsBirthDays = (students) => {
-//     const copyArr = [...students];
-//     let monthesArr = [['null', []], ['Jan', []], ['Feb', []], ['Mar', []], ['Apr', []], ['May', []], ['Jun', []], ['Jul', []], ['Aug', []], ['Sept', []], ['Oct', []], ['Nov', []], ['Dec', []]];
-
-//     const gradationByBirthday = {};
-
-//     copyArr.forEach((student) => {
-//         const dateArr = student.birthday.split('/');
-//         monthesArr.forEach((month, index) => {
-//             if(index == dateArr[0]) {
-//             month[1].push([student.name, dateArr[1]]);
-//                 month[1].sort((a, b) => b[1] - a[1]);
-//             }
-//         });
-//     });
-//     // console.log(monthesArr);
-//     monthesArr.forEach(elem => {
-//             if(elem[1].length > 0 &&  elem !== 'null') {
-//                 gradationByBirthday[elem[0]] = elem[1].map(name => name[0]);
-//             }
-//     });
-//     return gradationByBirthday;
-
-
-//  }
-
-// console.log(studentsBirthDays(students));
-
-
-const sorting = (a, b) => {
-    if (b.birthday.getDate() - a.birthday.getDate() !== 0) {
-        b.birthday.getDate() - a.birthday.getDate();
-    }
-    if (a.birthday.getDate() - b.birthday.getDate()) {
-        return a.birthday.getMonth() - b.birthday.getMonth();
-    }
-}
-
 export const studentsBirthDays = (students) => {
-    const copyArr = [...students];
+    const monthesArr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Dec'];
     let birthdayList = {};
-    const formatedDateArr = copyArr.map((student) => {
-        student.birthday = new Date(student.birthday);
-        return student;
-    }).sort(sorting);
-    formatedDateArr.forEach(student => {
-        student.birthday = student.birthday.toDateString()
-            .split(' ')
-            .splice(1, 3);
-        const month = student.birthday[0];
+    const copyArr = [...students].sort((a, b) => new Date(a.birthday).getDate() - new Date(b.birthday).getDate);
+        console.log(copyArr);
 
-        if (Object.keys(birthdayList).includes(month)) {
+    copyArr.forEach(student => {
+        const month =   monthesArr[new Date(student.birthday).getMonth()];
+        
+        if(birthdayList.hasOwnProperty(month)) {
             birthdayList[month].push(student.name);
         } else {
             birthdayList[month] = [student.name];
         }
-
     });
     console.log(birthdayList);
     return birthdayList;
-};
+}
 
 studentsBirthDays(students);
 
