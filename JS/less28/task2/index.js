6f834ddd96348c8a88252b984de83eb019d056d8
Anton <shotvideo25@gmail.com>
2020-07-16 13:59:47 +0300

    carrying

--- JS/less28/task2/index.js
+++ JS/less28/task2/index.js
@@ -1,33 +1,3 @@
-const favorites = ['id-6', 'id-17'];
-const tree = {
-    id: 'id-1',
-    name: 'Products',
-    nodes: [{
-            id: 'id-2',
-            name: "Food",
-            nodes: [{
-                id: 'id-6',
-                name: 'Drinks',
-                nodes: []
-            }]
-        },
-        {
-            id: 'id-17',
-            name: 'Vehicles',
-            nodes: []
-        }
-    ]
-};
-
-const markFav = (tree, fav) => {
-   const isFav = fav.includes(tree.id);
-   console.log(isFav, '  ', tree.id);
-
-   return {
-       ...tree,
-       isFav,
-       nodes: tree.nodes.map(childNodes => markFav(childNodes, favorites))
-   };
-};
-
-markFav(tree, favorites);
+export const mult = num1 => num2 => num1 * num2;
+export const twice = mult(2);
+export const triple = mult(3);
