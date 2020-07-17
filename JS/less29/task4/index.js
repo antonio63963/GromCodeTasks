4c417f65e30cd54e04e3e08c510632174c28c17a
Anton <shotvideo25@gmail.com>
2020-07-17 19:59:46 +0300

    jjj

--- JS/less29/task4/index.js
+++ JS/less29/task4/index.js
@@ -1,15 +1,16 @@
-export const delay = (delay, callback, context, ...args) => {
+ const delay = (delay, callback, context, ...args) => {
     if ([...args].length > 0) {
-        setTimeout(callback.call(context), delay);
+        setTimeout(() => callback.call(context), delay);
+    }
+    
+};
+
+const context = {
+    name: 'John',
+    text: 'задержка исполнения  ',
+    callback() {
+        console.log(`Mr. ${this.name} ${this.text}`);
     }
 };
 
-// const context = {
-//     name: 'John',
-//     text: 'задержка исполнения  ',
-//     callback() {
-//         console.log(`Mr. ${this.name} ${this.text}`);
-//     }
-// };
-
-// delay(1000, context.callback, context, 65, '34');
+delay(5000, context.callback, context, 65, '34');
