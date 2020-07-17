0db192c8b5e95ee7e3121bd8cc7ea95ad067dad2
Anton <shotvideo25@gmail.com>
2020-07-17 19:52:40 +0300

    hz

---
+++ JS/less29/task4/index.js
@@ -0,0 +1,15 @@
+const delay = (delay, callback, context, ...args) => {
+    if ([...args].length > 0) {
+        setTimeout(callback.call(context), delay);
+    }
+};
+
+const context = {
+    name: 'John',
+    text: 'задержка исполнения  ',
+    callback() {
+        console.log(`Mr. ${this.name} ${this.text}`);
+    }
+};
+
+delay(1000, context.callback, context, 65, '34');
