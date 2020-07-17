7498584bb5b2ac0f3c36d4c6a8936519fb28df12
Anton <shotvideo25@gmail.com>
2020-07-17 19:26:27 +0300

    hz

--- JS/less29/task1/index.js
+++ JS/less29/task1/index.js
@@ -26,4 +26,4 @@ const onImageLoaded = (error, imgElem) => {
 
 
 //test call
-addImage("https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg", onImageLoaded);
+// addImage("https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg", onImageLoaded);
