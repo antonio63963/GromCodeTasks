069913fff82d34c27e9c20387e2c83bb6e47a75e
Anton <shotvideo25@gmail.com>
2020-07-17 14:18:36 +0300

    asinc start

--- JS/less29/index.js
+++ JS/less29/index.js
@@ -1,4 +1,4 @@
-const addImage = (imgSrc, callback) => {
+export const addImage = (imgSrc, callback) => {
 
     //TODO write your code here
     const addImageElem = document.querySelector('.page');
