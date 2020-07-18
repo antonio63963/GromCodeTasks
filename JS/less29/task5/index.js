b06f4e6295f50bcf8fb7d70f0797da4722de5943
Anton <shotvideo25@gmail.com>
2020-07-18 12:31:20 +0300

    hz

---
+++ JS/less29/task5/index.js
@@ -0,0 +1,18 @@
+export { requestUserData, callback1 };
+
+function requestUserData(userId, callback) {
+    const random = Math.ceil(Math.random() * 3000);
+    setTimeout(() => callback(userId, 'Failed to load data'), random);
+
+
+}
+
+const callback1 = (id, text) => {
+    const result = id == 'broken' ? text : {
+        userId: id,
+        email: `${id}@example.com`
+    };
+    console.log(result);
+};
+// console.log(requestUserData('user777', callback1));
+// console.log(requestUserData('broken', callback1));
